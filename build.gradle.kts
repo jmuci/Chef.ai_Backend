7e9b51773ceb3be24ef26ad488eae987d84b0b11
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2025-09-10 09:06:36 +0200

    Add build deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ application {
 }
 
 dependencies {
+
     implementation(libs.logback.classic)
 
     implementation(libs.ktor.server.config.yaml)
@@ -22,6 +23,13 @@ dependencies {
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.status.pages)
     implementation(libs.ktor.server.thymeleaf)
+
+    // Database
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.dao)
+    implementation(libs.h2)
+    implementation(libs.postgresql)
 
     testImplementation(libs.json.path)
     testImplementation(libs.kotlin.test.junit)
