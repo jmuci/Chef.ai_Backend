7ef144d98b83e27cf1b7c29e75532eb23e216829
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2025-09-18 07:33:12 -0600

    Add JWT to ktor
    - Configure application
    - Add JwtConf and Service classes

--- src/main/kotlin/application/service/Application.kt
+++ src/main/kotlin/application/service/Application.kt
@@ -1,8 +1,13 @@
 package com.tenmilelabs.application.service
 
+import com.tenmilelabs.domain.service.AuthService
+import com.tenmilelabs.domain.service.JwtService
 import com.tenmilelabs.domain.service.RecipesService
+import com.tenmilelabs.infrastructure.auth.configureJwtAuth
 import com.tenmilelabs.infrastructure.database.PostgresRecipesRepository
+import com.tenmilelabs.infrastructure.database.PostgresUserRepository
 import com.tenmilelabs.infrastructure.database.RecipesRepository
+import com.tenmilelabs.infrastructure.database.UserRepository
 import com.tenmilelabs.infrastructure.database.configureDatabases
 import com.tenmilelabs.presentation.routes.configureRouting
 import io.ktor.server.application.*
@@ -14,11 +19,22 @@ fun main(args: Array<String>) {
 
 fun Application.module(
     recipeRepository: RecipesRepository = PostgresRecipesRepository(log),
+    userRepository: UserRepository = PostgresUserRepository(log)
 ) {
     val recipeRepository = recipeRepository
+    val userRepository = userRepository
+
+    // Configure JWT settings
+    val jwtSecret = environment.config.propertyOrNull("jwt.secret")?.getString() ?: "secret"
+    val jwtIssuer = environment.config.propertyOrNull("jwt.issuer")?.getString() ?: "http://0.0.0.0:8080"
+    val jwtAudience = environment.config.propertyOrNull("jwt.audience")?.getString() ?: "jwt-audience"
+
+    val jwtService = JwtService(jwtSecret, jwtIssuer, jwtAudience)
+    val authService = AuthService(userRepository, jwtService, log)
     val recipesService = RecipesService(recipeRepository, log)
 
     // Set up plugins
     configureDatabases()
-    configureRouting(recipeRepository = recipeRepository, recipesService)
+    configureJwtAuth(jwtService)
+    configureRouting(recipeRepository = recipeRepository, recipesService, authService)
 }
